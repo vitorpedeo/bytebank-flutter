0ffaf183d1d271803fb10980590ac9722519f370
Vitor Pereira <vitorpereiradeoli@gmail.com>
2022-07-10 15:49:40 -0300

    Módulo Implementando layout com Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,75 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Column(
-      children: const <Widget>[
-        Text(
-          'Hello World!',
-          textDirection: TextDirection.ltr,
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Transferências'),
         ),
-        Text(
-          'Bytebank',
-          textDirection: TextDirection.ltr,
+        body: ListaTransferencias(),
+        floatingActionButton: const FloatingActionButton(
+          onPressed: null,
+          child: Icon(Icons.add),
         ),
-        Expanded(child: FittedBox(fit: BoxFit.contain, child: FlutterLogo()))
-      ],
+      ),
     ),
   );
+}
+
+class ListaTransferencias extends StatelessWidget {
+  const ListaTransferencias({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const <Widget>[
+        ItemTransferencia(
+          transferencia: Transferencia(
+            valor: 100,
+            numeroConta: 124511,
+          ),
+        ),
+        ItemTransferencia(
+          transferencia: Transferencia(
+            valor: 200,
+            numeroConta: 637129,
+          ),
+        ),
+        ItemTransferencia(
+          transferencia: Transferencia(
+            valor: 350,
+            numeroConta: 988124,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia transferencia;
+
+  const ItemTransferencia({Key? key, required this.transferencia})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: const Icon(
+          Icons.monetization_on,
+          size: 40,
+        ),
+        title: Text(transferencia.valor.toString()),
+        subtitle: Text(transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  const Transferencia({required this.valor, required this.numeroConta});
 }
